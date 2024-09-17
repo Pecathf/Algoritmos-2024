5311d0279812854ba8c1aaffcb96ded2f00d18fd
Pecathf <167771326+Pecathf@users.noreply.github.com>
2024-09-17 07:31:00 -0400

    Add files via upload

--- sav.py
+++ sav.py
@@ -4,8 +4,8 @@ agua = int (input("Cantidad de tazas de agua ")) #El agua se da en tazas
 
 harina = int(input("Cantidad de tazas de harina ")) #la harina se da en tazas
 
-sal = int(input ("Cantidad de sal ",))/16/3 #La sal se convierte en cdas a tazas
-aceite = int(input("cantidad de aceite "))/16 #El aceite se convierte en agua
+sal = int(input ("Cantidad de sal ",))//16//3 #La sal se convierte en cdas a tazas
+aceite = int(input("cantidad de aceite "))//16 #El aceite se convierte en agua
 print ("el agua es", agua)
 print("la harina es",harina)
 print ("el aceite es", aceite)
@@ -17,5 +17,6 @@ budare = bol + aceite
 #se imprime el resultado
 print ("Con la cantidad de ingredientes salieron", budare)
 input("presione enter para terminar")
+
 
 
