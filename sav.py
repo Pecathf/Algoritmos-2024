ec6435acfadba00186d73f97edbce06c80f48e87
Pecathf <167771326+Pecathf@users.noreply.github.com>
2024-09-17 07:13:05 -0400

    Add files via upload

---
+++ sav.py
@@ -0,0 +1,21 @@
+
+#se declara los ingredientes
+agua = int (input("Cantidad de tazas de agua ")) #El agua se da en tazas
+
+harina = int(input("Cantidad de tazas de harina ")) #la harina se da en tazas
+
+sal = int(input ("Cantidad de sal ",))/16/3 #La sal se convierte en cdas a tazas
+aceite = int(input("cantidad de aceite "))/16 #El aceite se convierte en agua
+print ("el agua es", agua)
+print("la harina es",harina)
+print ("el aceite es", aceite)
+print ("El aceite es", sal)
+#comienza la preparacion de la arepa
+bol =agua + harina + sal
+budare = bol + aceite
+
+#se imprime el resultado
+print ("Con la cantidad de ingredientes salieron", budare)
+input("presione enter para terminar")
+
+
